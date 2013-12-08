8f4777f20edab297842fe12f6dfa12919e8ce4f9
Gene Wu <genewoo@gmail.com>
2013-12-08 14:07:30 +0800

    Initial checkin with a new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pressmen::Application.config.secret_token = '34c4adb815b005b0e0c604c18b7e6ae20e97dc99dacfd3e10612ecdcffe23e2166a017e5499b36f609b7e9f179e0bcaecf8f91b22070badca9cd76cfcab5e48b'
